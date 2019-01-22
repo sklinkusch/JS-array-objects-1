075ed5e94b878a9548fdf6afe29aad8b0a9c3c18
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-01-22 14:10:17 +0100

    returned books on sale as objects

---
+++ assets/js/main.js
@@ -0,0 +1,34 @@
+const books = [
+  {
+    title: "Dive into HTML5",
+    author: "Mark Pilgrim",
+    url: "http://diveinto.html5doctor.com/",
+    onSale: false
+  },
+  {
+    title: "Understanding Flexbox",
+    author: "Ohans Emmanuel",
+    url: "https://ohansemmanuel.github.io/uf_download.html",
+    onSale: true
+  },
+  {
+    title: "Maintenable CSS",
+    author: "Adam Silver",
+    url: "https://maintainablecss.com/",
+    onSale: false
+  },
+  {
+    title: "Eloquent JavaScript",
+    author: "Marijn Haverbeke",
+    url: "http://eloquentjavascript.net/",
+    onSale: true
+  }
+];
+function IsOnSale(myArray) {
+  myArray.forEach(element => {
+    if (element.onSale) {
+      console.log(element);
+    }
+  });
+}
+IsOnSale(books);
